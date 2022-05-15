f7b3db09368f97f5fd52bd28f3ffba6538fae6e4
viclmh <viclmh@umich.edu>
2022-05-15 17:21:57 -0400

    add new repository OrderRepository

--- dispatch/src/main/java/com/team4/dispatch/repository/OrderRepository.java
+++ dispatch/src/main/java/com/team4/dispatch/repository/OrderRepository.java
@@ -1,2 +1,10 @@
-package com.team4.dispatch.repository;public class OrderRepository {
+package com.team4.dispatch.repository;
+
+import com.team4.dispatch.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
