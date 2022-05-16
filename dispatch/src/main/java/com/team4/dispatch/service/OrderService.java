24b50add8f6939de22482a209a767d10c8d3ab56
viclmh <viclmh@umich.edu>
2022-05-16 14:25:05 -0400

    create order service

--- dispatch/src/main/java/com/team4/dispatch/service/OrderService.java
+++ dispatch/src/main/java/com/team4/dispatch/service/OrderService.java
@@ -1,29 +1,41 @@
 package com.team4.dispatch.service;
 
 import com.team4.dispatch.model.Order;
+import com.team4.dispatch.model.Robot;
 import com.team4.dispatch.repository.OrderRepository;
+import com.team4.dispatch.repository.RobotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
   private OrderRepository orderRepository;
-  private Order order;
+  private RobotRepository robotRepository;
 
   @Autowired
-  public OrderService(OrderRepository orderRepository, Order order) {
+  public OrderService(OrderRepository orderRepository, RobotRepository robotRepository) {
     this.orderRepository = orderRepository;
-    this.order = order;
+    this.robotRepository = robotRepository;
   }
 
-  public void add(Order order) {
+  public void addOrder(Order order) {
     orderRepository.save(order);
   }
 
-  @Transactional(isolation = Isolation.SERIALIZABLE)
-  public void deleteOrder(Long orderId) throws OrderNotExistException {
-    Order order = orderRepository.findById(orderId);
-    if (order == null) {
-      throw new OrderNotExistException("Order doesn't exist");
-    }
-    orderRepository.deleteById(orderId);
+  public void addRobot(Robot robot) {
+    robotRepository.save(robot);
   }
+
+
+
+//  @Transactional(isolation = Isolation.SERIALIZABLE)
+//  public void deleteOrder(Long orderId) throws OrderNotExistException {
+//    Order order = orderRepository.findById(orderId);
+//    if (order == null) {
+//      throw new OrderNotExistException("Order doesn't exist");
+//    }
+//    orderRepository.deleteById(orderId);
+//  }
 }
