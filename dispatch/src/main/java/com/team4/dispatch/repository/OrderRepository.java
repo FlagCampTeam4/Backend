510f5c2030e76085002f17b4696d38d90f8076a8
viclmh <viclmh@umich.edu>
2022-05-16 22:55:20 -0400

    update order repo

--- dispatch/src/main/java/com/team4/dispatch/repository/OrderRepository.java
+++ dispatch/src/main/java/com/team4/dispatch/repository/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.team4.dispatch.repository;
 
 import com.team4.dispatch.model.Order;
+import com.team4.dispatch.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByOrderID(Long id);
 
+    Order findByOrderIDAndGuest(Long id, User guest);
 }
