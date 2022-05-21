24c1aacf6740e22d6a15e14a51c161fbe2604be5
Lucas0717 <gangyupa@usc.edu>
2022-05-21 11:16:16 -0700

    update stationID to robot

--- dispatch/src/main/java/com/team4/dispatch/controller/OrderController.java
+++ dispatch/src/main/java/com/team4/dispatch/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.team4.dispatch.controller;
 
 import com.team4.dispatch.model.Order;
 import com.team4.dispatch.model.Robot;
+import com.team4.dispatch.model.Station;
 import com.team4.dispatch.model.User;
 import com.team4.dispatch.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +16,21 @@ import java.time.LocalDateTime;
 
 @RestController
 public class OrderController {
-  private OrderService orderService;
+    private OrderService orderService;
 
-  @Autowired
-  public OrderController(OrderService orderService) {
-    this.orderService = orderService;
-  }
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
-  @PostMapping("/order")
-  public void addOrder(@RequestBody Order order, Robot robot, Principal principal){
-      orderService.addRobot(robot);
+    @PostMapping("/order")
+    public void addOrder(@RequestBody Order order, Robot robot, Principal principal){
+        Station station = orderService.getStation(order.getStationID());
+        robot.setStationID(station);
+        orderService.addRobot(robot);
 
-      order.setGuest(new User.Builder().setUsername(principal.getName()).build());
-      order.setRobotID(robot);
-      orderService.addOrder(order);
-
-  }
+        order.setGuest(new User.Builder().setUsername(principal.getName()).build());
+        order.setRobotID(robot);
+        orderService.addOrder(order);
+    }
 }
