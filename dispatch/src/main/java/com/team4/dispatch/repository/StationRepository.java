b4a0f39a31fc012ae086cd8310803a6e0e2b919e
Lucas0717 <gangyupa@usc.edu>
2022-05-21 11:22:22 -0700

    add station interface

--- dispatch/src/main/java/com/team4/dispatch/repository/StationRepository.java
+++ dispatch/src/main/java/com/team4/dispatch/repository/StationRepository.java
@@ -1,4 +1,10 @@
 package com.team4.dispatch.repository;
 
-public interface StationRepository {
+import com.team4.dispatch.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StationRepository extends JpaRepository<Station, Integer> {
+    Station findByStationID(int stationID);
 }
